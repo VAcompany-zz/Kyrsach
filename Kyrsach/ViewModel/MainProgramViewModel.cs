8f1fc64945d4c005acb500dc2f883f3194e15c0f
Aksennik Vladislav <aksennik.vlad@gmail.com>
2022-04-24 23:30:23 +0300

    Добавьте файлы проекта.

---
+++ Kyrsach/ViewModel/MainProgramViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kyrsach.ViewModel
+{
+    public class MainProgramViewModel : BaseViewModel
+    {
+        private int _Balance = 150;
+        public int Balance
+        {
+            get => _Balance;
+            set => Set(ref _Balance, value);
+        }
+
+    }
+}
---
+++ Kyrsach/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kyrsach.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _Title = "VA_Crypto";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+     
+
+
+    }
+}
